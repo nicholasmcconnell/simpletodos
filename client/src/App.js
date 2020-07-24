4537867c6bade6cd682f88ad7deb435fc612427e
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-07-23 21:46:20 -0400

    removed localhost prefix from axios calls

--- client/src/App.js
+++ client/src/App.js
@@ -24,12 +24,12 @@ function App() {
       }
 
       const tokenRes = await Axios.post(
-        'http://localhost:5000/api/users/tokenIsValid',
+        '/api/users/tokenIsValid',
         null,
         { headers: { 'x-auth-token': token } }
       );
       if (tokenRes.data) {
-        const userRes = await Axios.get('http://localhost:5000/api/users/', {
+        const userRes = await Axios.get('/api/users/', {
           headers: { 'x-auth-token': token }
         });
         setUserData({
