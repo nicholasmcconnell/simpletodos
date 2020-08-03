db64548a1f261654d45966ab9c13fb91cde3798d
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-08-03 19:17:06 -0400

    create todo form created and home button in CreateTodo component

--- client/src/components/todo/CreateTodos.js
+++ client/src/components/todo/CreateTodos.js
@@ -6,6 +6,10 @@ import ErrorNotice from '../misc/ErrorNotice';
 
 export default function CreateTodos() {
 
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [youTubeUrl, setYouTubeUrl] = useState('');
+
     const { userData } = useContext(UserContext);
     const history = useHistory();
 
@@ -21,6 +25,33 @@ export default function CreateTodos() {
         <div className='page'>
             <div className='container'>
                 <h2>CreateTodos</h2>
+                <form className='form' onSubmit={submit}>
+                    <label htmlFor='todo-title'>Title</label>
+                    <input
+                        id='todo-title'
+                        type='text'
+                        onChange={e => setTitle(e.target.value)}
+                    />
+
+                    <label htmlFor='todo-description'>Description</label>
+                    <textarea
+                        id='todo-description'
+                        name='todo-description'
+                        onChange={e => setDescription(e.target.value)}
+                    />
+
+                    <label htmlFor='todo-youtubeurl'></label>
+                    <input
+                        id='todo-youtubeurl'
+                        type='url'
+                        onChange={e => setYouTubeUrl(e.target.value)}
+                    />
+
+                    <input type='submit' value='todo-form' />
+                </form>
+                <Link to='/'>
+                    <button className='type-button' value='Home'>Home</button>
+                </Link>
             </div>
         </div>
     )
