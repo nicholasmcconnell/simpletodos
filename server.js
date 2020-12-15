1bfb43880a3c18d7320b894f7773556124081124
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-12-14 21:46:45 -0500

    Successfull creating of todo routes to create Todos

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(routes);
 //"mongodb://localhost/todos" || 
 //|| 
 mongoose.connect(
-    (process.env.MONGODB_URI), {
+    ("mongodb://localhost/todos" || process.env.MONGODB_URI), {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
